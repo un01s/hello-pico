b922f6b976327359ed11acb457150bcfd4d91b0e
un01s <96022876+un01s@users.noreply.github.com>
2025-09-07 13:34:25 -0600

    snowflake-start

--- ws2812.c
+++ ws2812.c
@@ -26,7 +26,7 @@
  *
  */
 #define IS_RGBW false
-#define NUM_PIXELS 150
+#define NUM_PIXELS 75
 
 #ifdef PICO_DEFAULT_WS2812_PIN
 #define WS2812_PIN PICO_DEFAULT_WS2812_PIN
@@ -41,103 +41,137 @@
 #endif
 
 static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
+  pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
 }
 
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
+  return ((uint32_t) (r) << 8) |
+         ((uint32_t) (g) << 16) |
+         (uint32_t) (b);
 }
 
 static inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            ((uint32_t) (w) << 24) |
-            (uint32_t) (b);
+  return ((uint32_t) (r) << 8) |
+         ((uint32_t) (g) << 16) |
+         ((uint32_t) (w) << 24) |
+         (uint32_t) (b);
 }
 
 void pattern_snakes(PIO pio, uint sm, uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(pio, sm, 0);
-    }
+  for (uint i = 0; i < len; ++i) {
+    uint x = (i + (t >> 1)) % 64;
+    if (x < 10)
+      put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
+    else if (x >= 15 && x < 25)
+      put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
+    else if (x >= 30 && x < 40)
+      put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
+    else
+      put_pixel(pio, sm, 0);
+  }
 }
 
 void pattern_random(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand());
+  if (t % 8)
+    return;
+  for (uint i = 0; i < len; ++i)
+    put_pixel(pio, sm, rand());
 }
 
 void pattern_sparkle(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand() % 16 ? 0 : 0xffffffff);
+  if (t % 8)
+    return;
+  for (uint i = 0; i < len; ++i)
+    put_pixel(pio, sm, rand() % 16 ? 0 : 0xffffffff);
 }
 
 void pattern_greys(PIO pio, uint sm, uint len, uint t) {
-    uint max = 100; // let's not draw too much current!
-    t %= max;
-    for (uint i = 0; i < len; ++i) {
-        put_pixel(pio, sm, t * 0x10101);
-        if (++t >= max) t = 0;
+  uint max = 100; // let's not draw too much current!
+  t %= max;
+  for (uint i = 0; i < len; ++i) {
+    put_pixel(pio, sm, t * 0x10101);
+    if (++t >= max) t = 0;
+  }
+}
+
+void pattern_rainbow(PIO pio, uint sm, uint len, uint t) {
+  uint8_t red = 0;
+  uint8_t green = 0;
+  uint8_t blue = 0;
+  int rgbValues[] = {255, 0, 0};
+  int upIndex = 0, downIndex = 1;
+ 
+  for (uint i = 0; i < 100; i++) {
+  for (uint j = 0; j < len; j++) {
+    rgbValues[upIndex] += 1;
+    rgbValues[downIndex] -= 1;
+
+    if(rgbValues[upIndex] > 255){
+      rgbValues[upIndex] = 255;
+      upIndex = upIndex + 1;
+
+      if(upIndex > 2){
+        upIndex = 0;
+      }
     }
+
+    if(rgbValues[downIndex] < 0){
+      rgbValues[downIndex] = 0;
+      downIndex = downIndex + 1;
+
+      if(downIndex > 2){
+        downIndex = 0;
+      }
+    }
+
+    put_pixel(pio, sm, urgb_u32((uint8_t)(255-rgbValues[0]), (uint8_t)(255-rgbValues[1]), (uint8_t)(255-rgbValues[2])));
+  }
+  }
 }
 
 typedef void (*pattern)(PIO pio, uint sm, uint len, uint t);
 const struct {
-    pattern pat;
-    const char *name;
+  pattern pat;
+  const char *name;
 } pattern_table[] = {
-        {pattern_snakes,  "Snakes!"},
-        {pattern_random,  "Random data"},
-        {pattern_sparkle, "Sparkles"},
-        {pattern_greys,   "Greys"},
+  {pattern_snakes,  "Snakes!"},
+  {pattern_random,  "Random data"},
+  {pattern_sparkle, "Sparkles"},
+  {pattern_greys,   "Greys"},
+  {pattern_rainbow, "Rainbow"},
 };
 
 int main() {
-    //set_sys_clock_48();
-    stdio_init_all();
-    printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
+  //set_sys_clock_48();
+  stdio_init_all();
+  printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
 
-    // todo get free sm
-    PIO pio;
-    uint sm;
-    uint offset;
+  // todo get free sm
+  PIO pio;
+  uint sm;
+  uint offset;
 
-    // This will find a free pio and state machine for our program and load it for us
-    // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
-    // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
-    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
-    hard_assert(success);
+  // This will find a free pio and state machine for our program and load it for us
+  // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
+  // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
+  bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
+  hard_assert(success);
 
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+  ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    int t = 0;
-    while (1) {
-        int pat = rand() % count_of(pattern_table);
-        int dir = (rand() >> 30) & 1 ? 1 : -1;
-        puts(pattern_table[pat].name);
-        puts(dir == 1 ? "(forward)" : "(backward)");
-        for (int i = 0; i < 1000; ++i) {
-            pattern_table[pat].pat(pio, sm, NUM_PIXELS, t);
-            sleep_ms(10);
-            t += dir;
-        }
+  int t = 0;
+  while (1) {
+    int pat = rand() % count_of(pattern_table);
+    int dir = (rand() >> 30) & 1 ? 1 : -1;
+    puts(pattern_table[pat].name);
+    puts(dir == 1 ? "(forward)" : "(backward)");
+    for (int i = 0; i < 1000; ++i) {
+      pattern_table[pat].pat(pio, sm, NUM_PIXELS, t);
+      sleep_ms(10);
+      t += dir;
     }
+  }
 
-    // This will free resources and unload our program
-    pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
+  // This will free resources and unload our program
+  pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
 }
